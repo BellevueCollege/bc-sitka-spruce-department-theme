73bb6daf7c03ea63c1dbc6d76ee234ad2d069b30
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-06-26 12:18:13 -0700

    Add Bootstrap CSS

--- functions.php
+++ functions.php
@@ -14,3 +14,21 @@ function register_menus() {
 	);
 }
 add_action( 'init', __NAMESPACE__ . '\register_menus' );
+
+
+/**
+ * Enqueue Assets
+ * 
+ * Enqueue CSS and JS assets.
+ */
+function enqueue_assets() {
+	$asset = include get_parent_theme_file_path( 'assets/dist/css/main.asset.php' );
+
+	wp_enqueue_style(
+		'bc-sitka-spruce-style',
+		get_parent_theme_file_uri( 'assets/dist/css/main.css' ),
+		$asset['dependencies'],
+		$asset['version']
+	);
+}
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
