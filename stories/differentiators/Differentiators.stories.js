e717cb072c374ee6abfe59f18c9693c25ebee76d
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-07-03 11:36:07 -0700

    Fix Differentiators Story

--- stories/differentiators/Differentiators.stories.js
+++ stories/differentiators/Differentiators.stories.js
@@ -1,34 +1,57 @@
 import twigDifferentiators from "./differentiators.twig";
+import twigDifferentiatorGroup from "./differentiator-group.twig";
+import twigDifferentiator from "./differentiator.twig";
+
 import { differentiatorIcon, differentiatorText, differentiatorImage } from "./differentiator.data.js";
 
 export default {
     title: "Stories/Differentiators",
-    component: "differentiators",
+    component: twigDifferentiators,
     tags: ['autodocs'],
 };
 
 
-const Template = ( { 
-    heading,
-    subheading,
-    link,
-    differentiators
+const TemplateDifferentiators = ({ content }) => twigDifferentiators({ content });
+const TemplateDifferentiatorGroup = ({ content }) => twigDifferentiatorGroup({ content });
+const TemplateDifferentiator = ( { 
+    top_layout,
+    top_text,
+    top_superscript,
+    top_icon,
+    top_image,
+    title,
+    text,
+    link
  }) =>
-    twigDifferentiators({
-        heading,
-        subheading,
-        link,
-        differentiators
+    twigDifferentiator({
+        top_layout,
+        top_text,
+        top_superscript,
+        top_icon,
+        top_image,
+        title,
+        text,
+        link
     });
 
-export const Default = Template.bind({});
+
+
+export const Default = TemplateDifferentiators.bind({});
 Default.args = {
-    heading: 'What Makes Us Different',
-    subheading: '<p>Bellevue College is an exciting place to go to school</p>',
-    link: '<a href="https://www.bellevuecollege.edu" class="link-arrow">Bellevue College</a>',
-    differentiators: [
-        differentiatorIcon,
-        differentiatorText,
-        differentiatorImage
-    ]
+    content: twigDifferentiatorGroup({ 
+        content: [ 
+            twigDifferentiator({ ...differentiatorText }),
+            twigDifferentiator({ ...differentiatorIcon }),
+            twigDifferentiator({ ...differentiatorImage })
+        ].join()
+    })
+};
+
+export const DifferentiatorGroupContainer = TemplateDifferentiatorGroup.bind({});
+DifferentiatorGroupContainer.args = {
+    content: [ 
+        twigDifferentiator({ ...differentiatorText }),
+        twigDifferentiator({ ...differentiatorIcon }),
+        twigDifferentiator({ ...differentiatorImage })
+    ].join()
 };
