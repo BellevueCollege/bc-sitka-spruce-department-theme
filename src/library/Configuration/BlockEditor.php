41ea1a2a2fa42dacc55640a0ca95d1aed770634f
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-07-10 12:22:52 -0700

    Disable Editor Modifications from B22
    Not needed right now- may re-enable some or all later

--- src/library/Configuration/BlockEditor.php
+++ src/library/Configuration/BlockEditor.php
@@ -98,15 +98,15 @@ class BlockEditor implements BlockEditorInterface {
 
   public function __construct() {
     // The presence of theme.json implies this is true, so manually opt out.
-    if (!$this->useFullSiteEditing) {
-      remove_theme_support('block-templates');
-    }
-    add_filter('allowed_block_types_all', [$this, 'setupAllowedBlocks'], 10, 2);
-    add_filter('block_categories_all', [$this, 'setupBlockGroups'], 10, 2);
-    add_filter('use_block_editor_for_post', [$this, 'setupBlockEditorForPost'], 10, 2);
-    add_action('acf/init', [$this, 'setupAcfBlocks'], 10, 1);
+    // if (!$this->useFullSiteEditing) {
+    //   remove_theme_support('block-templates');
+    // }
+    // add_filter('allowed_block_types_all', [$this, 'setupAllowedBlocks'], 10, 2);
+    // add_filter('block_categories_all', [$this, 'setupBlockGroups'], 10, 2);
+    // add_filter('use_block_editor_for_post', [$this, 'setupBlockEditorForPost'], 10, 2);
+    // add_action('acf/init', [$this, 'setupAcfBlocks'], 10, 1);
     add_action('enqueue_block_editor_assets', [$this, 'setupBlockEditorStylesheet'], 99, 0);
-    add_action('enqueue_block_editor_assets', [$this, 'setupAllowedBlockVariations'], 10, 0);
+    //add_action('enqueue_block_editor_assets', [$this, 'setupAllowedBlockVariations'], 10, 0);
   }
 
   /**
@@ -329,8 +329,8 @@ class BlockEditor implements BlockEditorInterface {
    */
   public function setupAllowedBlockVariations(): void {
     wp_enqueue_script(
-      'bellevue-2022-block-variations-blacklist',
-      get_template_directory_uri() . '/src/Configuration/block-variations-blacklist.js',
+      'sitka-spruce-block-variations-blacklist',
+      get_template_directory_uri() . '/src/library/Configuration/block-variations-blacklist.js',
       ['wp-blocks', 'wp-dom-ready', 'wp-edit-post'],
   );
   }
