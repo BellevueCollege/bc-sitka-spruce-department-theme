c86a7187aed326ca383f398a52456d1cc0d2225a
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-07-15 16:49:51 -0700

    Add Sample Menu Markup to Header Story

--- stories/header/Header.stories.js
+++ stories/header/Header.stories.js
@@ -1,6 +1,31 @@
 import bcHeader from "./header.twig";
 import logo from "/assets/img/logo-header.svg";
 
+
+
+// Sample menu output from WordPress
+const menu = `<ul class="menu"><li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-145 ">
+<a href="#" target="_self">Menu Item 1</a>
+</li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-146 ">
+<a href="#" target="_self">Menu Item 2</a>
+<ul class="menu">
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-148 ">
+<a href="#" target="_self">Submenu Item 1</a>
+</li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-149 ">
+<a href="#" target="_self">Submenu Item 2</a>
+</li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-147 ">
+<a href="#" target="_self">Submenu Item 3</a>
+</li>
+</ul>
+</li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150 ">
+<a href="#" target="_self">Menu Item 3</a>
+</li>
+</ul>`;
+
 export default {
     title: "Stories/Header",
     component: "header",
@@ -35,5 +60,5 @@ Default.args = {
     logo_url: logo,
     search_expanded: false,
     menu_expanded: false,
-    menu: ''
+    menu: menu
 };
