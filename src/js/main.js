a3cd55b860d8e0e245374c2c8f756292920dc6e9
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-07-16 10:02:03 -0700

    Improve Menu Display on Mobile
    Fix behavior, spacing, and background colors

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import AccessibleMenu from './modules/accessible-menu';
+import ButtonToggle from './modules/button-toggle';
 
 (() => {
 
@@ -7,5 +8,11 @@ import AccessibleMenu from './modules/accessible-menu';
      */
     const accessibleMainMenu = new AccessibleMenu();
     accessibleMainMenu.add('.main-menu').run();
-    accessibleMainMenu.add('.utility-menu').run();
+    //accessibleMainMenu.add('.utility-menu').run();
+
+    /**
+     * Global Button Toggle controlled by data attrs.
+     */
+    const globalButtonToggle = new ButtonToggle();
+    globalButtonToggle.add('.button-toggle').run();
 })()
