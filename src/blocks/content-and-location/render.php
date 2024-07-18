4765b2c95a5d2e8addf747cfab474c71e89f5adb
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-07-18 12:57:22 -0700

    Content and Location Block
    Create block for use on homepage, containing a narrow content area and a location sidebar.

---
+++ src/blocks/content-and-location/render.php
@@ -0,0 +1,19 @@
+<?php
+use Timber\Timber;
+
+$context = Timber::context();
+
+$context['content']          = $content;
+$context['display_location'] = get_field( 'display_location_card', 'option' );
+$image                       = get_field( 'location_image', 'option' );
+if ( $image ) {
+	$context['image']['src'] = $image['url'];
+	$context['image']['alt'] = $image['alt'];
+}
+$context['location']    = get_field( 'location', 'option' );
+$context['hours']       = get_field( 'hours', 'option' );
+$context['contact_url'] = get_field( 'contact_page_url', 'option' );
+
+
+// Render Twig Template
+Timber::render( '/views/blocks/content-and-location.twig', $context );
