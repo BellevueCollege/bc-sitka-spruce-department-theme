f49f0c4b898c03022acb3fd6cdca6ae5619ff2e0
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-07-24 11:26:27 -0700

    Fix Homepage Pattern
    Update Pattern to insert with proper markup

--- patterns/page-homepage.php
+++ patterns/page-homepage.php
@@ -2,12 +2,18 @@
 /**
  * Title: Homepage Layout
  * Slug: bc-sitka-spruce/page-homepage
- * Categories: page
+ * Categories: page, featured
  * Keywords: starter
  * Block Types: core/post-content
  * Post Types: page, wp_template
- * Viewport width: 1400
+ * Viewport width: 1445
  */
 ?>
 
 <!-- wp:bc-sitka-spruce/template-homepage {"lock":{"move":true,"remove":true}} -->
+<div class="wp-block-bc-sitka-spruce-template-homepage homepage-intro alignfull">
+</div>
+<!-- /wp:bc-sitka-spruce/template-homepage -->
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
