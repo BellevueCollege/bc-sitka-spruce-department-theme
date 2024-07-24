ab58da7462c3c43f50749d102ac8d147e3435605
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-07-24 14:30:05 -0700

    Disable Group Block Variations

--- src/library/Configuration/block-variations-blacklist.js
+++ src/library/Configuration/block-variations-blacklist.js
@@ -2,6 +2,8 @@
  * @file
  * Block variations blacklist.
  */
-// wp.domReady(() => {
-//   wp.blocks.unregisterBlockVariation('core/group', 'group-row');
-// });
+wp.domReady(() => {
+  wp.blocks.unregisterBlockVariation('core/group', 'group-row');
+  wp.blocks.unregisterBlockVariation('core/group', 'group-stack');
+  wp.blocks.unregisterBlockVariation('core/group', 'group-grid');
+});
