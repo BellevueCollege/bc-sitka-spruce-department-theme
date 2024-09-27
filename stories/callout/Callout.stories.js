15533c96659b4930beeddda679471347dba61f74
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-09-27 08:25:31 -0700

    Add Info Style to Callout Story

--- stories/callout/Callout.stories.js
+++ stories/callout/Callout.stories.js
@@ -1,5 +1,7 @@
 import twigCallout from "./callout.twig";
 
+import '/assets/dist/blocks/callout/style-index.css';
+
 export default {
     title: "Stories/Callout",
     component: "callout",
@@ -7,8 +9,9 @@ export default {
 };
 
 
-const Template = ( { wrapper_classes, heading_tag, title, text, links }) =>
+const Template = ( { callout_type,wrapper_classes, heading_tag, title, text, links }) =>
     twigCallout({
+		callout_type,
 		wrapper_classes,
 		heading_tag,
         title,
@@ -18,6 +21,17 @@ const Template = ( { wrapper_classes, heading_tag, title, text, links }) =>
 
 export const Default = Template.bind({});
 Default.args = {
+	callout_type: 'callout-light',
+	wrapper_classes: '',
+	heading_tag: 'h2',
+    title: 'Spring Quarter Starts Monday!',
+    text: '<p>Get ready to celebrate the new year with our new program. </p>',
+    links: "<ul><li><a href='#' class='link-arrow'>Learn more</a></li><li><a href='#' class='link-arrow'>Another Link</a></li></ul>"
+};
+
+export const LightBlue = Template.bind({});
+LightBlue.args = {
+	callout_type: 'callout-info',
 	wrapper_classes: '',
 	heading_tag: 'h2',
     title: 'Spring Quarter Starts Monday!',
