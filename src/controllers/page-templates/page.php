5d202d62849ca06d5463f8aa3e387113b23751af
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-10-11 15:37:44 -0700

    Add Basic Breadcrumbs Functionality
    Add to page template and style breadcrumbs

--- src/controllers/page-templates/page.php
+++ src/controllers/page-templates/page.php
@@ -1,7 +1,12 @@
 <?php
 use Timber\Timber;
+use BcSitkaSpruce\Library\Theme;
+
 global $post;
+
 $context = Timber::context();
+
+$context['breadcrumbs'] = Theme::breadcrumbs()->getItems( 2 );
 $context['post'] = Timber::get_post();
 $context['header_image'] = get_field( 'header_image' ) ?
 	wp_get_attachment_image( get_field( 'header_image' ), 'full', false, array( 'class' => 'img-fluid rounded' ) ) : null;
