bafda9bc0de3d069a6aba6c822c80b4183fee8d4
Jaehong Lee <jaehong.lee@bellevuecollege.edu>
2024-10-23 09:45:35 -0700

    Remove commented area and Format the code block

--- stories/listing-section/ListingSection.stories.js
+++ stories/listing-section/ListingSection.stories.js
@@ -1,75 +1,66 @@
-import twigListingSection from "./listing-section.twig";
-import twigListItem from "./list-item.twig";
+import twigListingSection from './listing-section.twig';
+import twigListItem from './list-item.twig';
 import '/assets/dist/blocks/listing-section/style-index.css';
 export default {
-    title: "Stories/Listing Section",
-    component: "listing-section",
-    tags: ['autodocs'],
+	title: 'Stories/Listing Section',
+	component: 'listing-section',
+	tags: ['autodocs'],
 };
 
-
-
-const Template = ( {
-	title,
-	description,
-	list_items,
- }) =>
-    twigListingSection({
+const Template = ({ title, description, list_items }) =>
+	twigListingSection({
 		title,
 		description,
-		list_items
-    });
+		list_items,
+	});
 
 export const Default = Template.bind({});
 Default.args = {
 	title: 'Listing Section',
-	description: 'Optional description of this section. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
+	description:
+		'Optional description of this section. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
 	list_items: [
 		twigListItem({
-			title: "Item 1",
-			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.",
+			title: 'Item 1',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
 			links: [
 				{
-					title: "Link 1",
-					url: "#",
+					title: 'Link 1',
+					url: '#',
 				},
 				{
-					title: "Link 2",
-					url: "#",
+					title: 'Link 2',
+					url: '#',
 				},
 				{
-					title: "Link 3",
-					url: "#",
-				}
+					title: 'Link 3',
+					url: '#',
+				},
 			],
-/* 			button: {
-				title: "Button",
-				url: "#",
-			}, */
+
 			image: '<img src="https://placehold.co/360x240" class="img-fluid rounded" alt="Placeholder Image">',
 		}),
 		twigListItem({
-			title: "Item 2",
-			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.",
+			title: 'Item 2',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
 			links: [
 				{
-					title: "Link 1",
-					url: "#",
+					title: 'Link 1',
+					url: '#',
 				},
 				{
-					title: "Link 2",
-					url: "#",
+					title: 'Link 2',
+					url: '#',
 				},
 				{
-					title: "Link 3",
-					url: "#",
-				}
+					title: 'Link 3',
+					url: '#',
+				},
 			],
-/* 			button: {
-				title: "Button",
-				url: "#",
-			}, */
+
 			image: '<img src="https://placehold.co/360x240" class="img-fluid rounded" alt="Placeholder Image">',
-		})
-	].join('')
+		}),
+	].join(''),
 };
