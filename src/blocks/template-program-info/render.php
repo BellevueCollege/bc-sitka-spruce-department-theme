84a4d7bb588dcacb2e9dc1ac74f5908fa3128ff8
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-10-24 16:00:25 -0700

    Fix Call to Story from Block
    Story was not rendering due to issue with story path

--- src/blocks/template-program-info/render.php
+++ src/blocks/template-program-info/render.php
@@ -10,6 +10,5 @@ if ( ! array_key_exists( 'post', $context ) ) {
 }
 
 $program = Program::get_single_from_core_by_title( $context['post']->post_title );
-
 // Render Twig Template
-Timber::render( '/stories/program-information/program-information.twig', array_merge( $context, $program ) ); // Merge in the content from the core
+Timber::render( '/stories/program-info/program-info.twig', array_merge( $context, $program ) ); // Merge in the content from the core
