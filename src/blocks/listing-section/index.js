08921a6b9a056858efe291119f9b6eda6f862e4f
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-10-25 15:46:23 -0700

    Finalize Listing Section Blocks
    - Connect Listing Section block to Story
    - Add Listing Section List Item story

--- src/blocks/listing-section/index.js
+++ src/blocks/listing-section/index.js
@@ -1,11 +1,7 @@
 import { __ } from "@wordpress/i18n";
 import { registerBlockType } from "@wordpress/blocks";
 import { useBlockProps } from "@wordpress/block-editor";
-import { InnerBlocks } from "@wordpress/block-editor";
-import { RawHTML } from "@wordpress/element";
-import { useState } from "@wordpress/element";
-import { Disabled } from "@wordpress/components";
-import { RichText } from "@wordpress/block-editor";
+import { InnerBlocks, RichText } from "@wordpress/block-editor";
 
 import "./style.scss";
 import "./editor.scss";
@@ -14,9 +10,44 @@ import { Card, CardBody, CardHeader } from "@wordpress/components";
 
 registerBlockType("bc-sitka-spruce/listing-section", {
 	edit: function (props) {
+		const {
+			attributes: { title, description },
+			setAttributes,
+			isSelected,
+		} = props;
 		const blockProps = useBlockProps({
-			className: "section listing-section alignwide",
+			className: "section listing-section alignfull",
 		});
+
+		return (
+			<div {...blockProps}>
+				<div className="container-xl">
+					<RichText
+						tagName="h2"
+						className="section-heading__heading"
+						placeholder={__("Title of Section (required)...", "bc-sitka-spruce")}
+						value={title}
+						onChange={(title) => setAttributes({ title })}
+						identifier="title"
+						allowedFormats={[]}
+						disableLineBreaks={true}
+					/>
+					<RichText
+						tagName="p"
+						className="section-heading__subheading"
+						placeholder={__("Description (optional)...", "bc-sitka-spruce")}
+						value={description}
+						onChange={(description) => setAttributes({ description })}
+						identifier="description"
+						allowedFormats={[]}
+					/>
+
+					<InnerBlocks
+						renderAppender={ InnerBlocks.ButtonBlockAppender }
+					/>
+				</div>
+			</div>
+		);
 	},
 	save: function () {
 		return <InnerBlocks.Content />;
--- src/blocks/listing-section/render.php
+++ src/blocks/listing-section/render.php
@@ -6,6 +6,7 @@ $context = Timber::context();
 // Rich Text Fields
 $context['title']       = $attributes['title'];
 $context['description'] = $attributes['description'];
+$context['list_items']  = $content;
 
 // Render Twig Template
 Timber::render( '/stories/listing-section/listing-section.twig', $context );
