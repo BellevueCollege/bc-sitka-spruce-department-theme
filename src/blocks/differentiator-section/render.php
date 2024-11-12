a60a3096e4e7406bc289150222810670f35882de
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-11-12 15:42:10 -0800

    Escape Differentiator Section Output

--- src/blocks/differentiator-section/render.php
+++ src/blocks/differentiator-section/render.php
@@ -6,12 +6,11 @@ if ( ! isset( $content ) ) {
 }
 
 $context = Timber::context();
-$context['title'] = $attributes['title'];
-$context['description'] = $attributes['description'];
-$context['linkTitle'] = $attributes['linkTitle'];
-$context['linkUrl'] = $attributes['linkUrl'];
-
-$context['content'] = $content;
+$context['title']       = esc_html( $attributes['title'] ?? '' );
+$context['description'] = wp_kses_post( $attributes['description'] ?? '' );
+$context['linkTitle']   = esc_html( $attributes['linkTitle'] ?? '' );
+$context['linkUrl']     = esc_url( $attributes['linkUrl'] ?? '' );
+$context['content']     = $content;
 
 // Render Twig Template
 Timber::render( get_template_directory() . '/stories/differentiators/differentiators.twig', $context );
