36f43aaf211f7b5f593f0fb385e8ad4276bb339b
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-11-12 13:42:20 -0800

    Add Starter Template for Division Homepage

---
+++ patterns/page-homepage-division.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Title: Division Homepage Layout
+ * Slug: bc-sitka-spruce/page-homepage-division
+ * Categories: page, featured
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page, wp_template
+ * Viewport width: 1445
+ */
+?>
+
+<!-- wp:bc-sitka-spruce/template-homepage {"lock":{"move":true,"remove":true}} -->
+<div class="wp-block-bc-sitka-spruce-template-homepage homepage-intro alignfull"><!-- wp:bc-sitka-spruce/hero-image {"name":"bc-sitka-spruce/hero-image","data":{"hero_image":"","_hero_image":"field_66a037a5b53bb"},"mode":"edit"} /-->
+
+<!-- wp:bc-sitka-spruce/content-and-location -->
+<!-- wp:mayflower-blocks/button {"buttonText":"Optional Button","isBootstrap5":true,"lock":{"move":false,"remove":false}} -->
+<div class="wp-block-mayflower-blocks-button"><a class="btn btn-primary   disabled" aria-disabled="true">Optional Button</a></div>
+<!-- /wp:mayflower-blocks/button -->
+
+<!-- wp:gravityforms/form {"formId":"","inputPrimaryColor":"#204ce5","lock":{"move":false,"remove":false}} /-->
+<!-- /wp:bc-sitka-spruce/content-and-location --></div>
+<!-- /wp:bc-sitka-spruce/template-homepage -->
+
+<!-- wp:bc-sitka-spruce/announcement-banner {"name":"bc-sitka-spruce/announcement-banner","data":{"field_66e4c35bcf5ea":"","field_66e4c38bcf5eb":"","field_66e4c50d2ef34":"","field_66e4c39ecf5ec":{"title":"","url":"","target":""},"field_66e4c40bcf5ed":""},"mode":"auto"} /-->
+
+<!-- wp:bc-sitka-spruce/card-section {"title":"Our Departments/Sub-units"} -->
+<!-- wp:bc-sitka-spruce/card-section-card /-->
+
+<!-- wp:bc-sitka-spruce/card-section-card -->
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+<!-- /wp:bc-sitka-spruce/card-section-card -->
+
+<!-- wp:bc-sitka-spruce/card-section-card /-->
+<!-- /wp:bc-sitka-spruce/card-section -->
+
+<!-- wp:bc-sitka-spruce/listing-section {"title":"Our Services/Facilities"} /-->
+
+<!-- wp:bc-sitka-spruce/differentiators -->
+<!-- wp:bc-sitka-spruce/section-heading {"name":"bc-sitka-spruce/section-heading","data":{"field_6684655a0c09f":"","field_668464f00c09e":"","field_668465680c0a0":"","field_668465990c0a1":{"title":"","url":"","target":""}},"mode":"preview"} /-->
+
+<!-- wp:bc-sitka-spruce/differentiator-group -->
+<!-- wp:bc-sitka-spruce/differentiator /-->
+
+<!-- wp:bc-sitka-spruce/differentiator /-->
+
+<!-- wp:bc-sitka-spruce/differentiator /-->
+<!-- /wp:bc-sitka-spruce/differentiator-group -->
+<!-- /wp:bc-sitka-spruce/differentiators -->
+
+<!-- wp:bc-sitka-spruce/checkerboard-section {"name":"bc-sitka-spruce/checkerboard-section","data":{"field_62ff9d3385358":""},"mode":"auto"} /-->
+
+<!-- wp:paragraph -->
+<p><strong>PROFILES SECTION BLOCK GOES HERE!</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:bc-sitka-spruce/body-section -->
+<!-- wp:bc-sitka-spruce/body-section-content -->
+<!-- wp:heading -->
+<h2 class="wp-block-heading"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+<!-- /wp:bc-sitka-spruce/body-section-content -->
+
+<!-- wp:bc-sitka-spruce/callout {"name":"bc-sitka-spruce/callout","data":{"field_66b1334b05e6b":"0"},"mode":"preview"} /-->
+<!-- /wp:bc-sitka-spruce/body-section -->
+
+<!-- wp:bc-sitka-spruce/news-feature-core /-->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
