81ef23da2b27452310c691ac811e577580f115e2
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-11-14 08:05:48 -0800

    Improve Error Handling for Support and Dept Features

--- src/blocks/department-feature/render.php
+++ src/blocks/department-feature/render.php
@@ -5,7 +5,8 @@ $context                  = Timber::context();
 $context['heading']       = esc_html( $attributes['title'] );
 $context['subheading']    = esc_html( $attributes['description'] );
 $context['department_id'] = esc_attr( $attributes['departmentId'] );
-$context                  = array_merge( $context, Department::get_single_from_core( $attributes['departmentId'] ) ); // Merge in the content from the core
+$dept                     = Department::get_single_from_core( $attributes['departmentId'] );
+$context                  = is_array( $dept ) ? array_merge( $context, Department::get_single_from_core( $attributes['departmentId'] ) ) : $context; // Merge in the content from the core
 
 if ( $context['department_id'] ) {
 	Timber::render( '/stories/department-feature/department-feature.twig', $context );
