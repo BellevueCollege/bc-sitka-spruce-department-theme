564315978a005a1965a4e16a43ca955375d3a6b6
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-12-04 13:40:36 -0800

    Initial test baselines for Nightwatch

--- tests/homepage.js
+++ tests/homepage.js
@@ -1,18 +1,34 @@
 describe('Homepage', function () {
-	//before(browser => browser.navigateTo('https://bcqabackstopjs.kinsta.cloud/sitka-default/?cache=none'));
-	it('Sock UI appears correct', function (browser) {
-		browser
-			.navigateTo('https://bcqabackstopjs.kinsta.cloud/sitka-default/?cache=none')
-			.waitForElementVisible('aside.sock.sock-standard')
-			.verify.screenshotIdenticalToBaseline('aside.sock.sock-standard')
-			.end();
-	});
+
+	before(browser => browser.navigateTo('https://bcqabackstopjs.kinsta.cloud/sitka-default/?cache=none'));
+
 	it('Footer UI appears correct', function (browser) {
 		browser
-			.navigateTo('https://bcqabackstopjs.kinsta.cloud/sitka-default/?cache=none')
 			.waitForElementVisible('footer.footer')
-			.verify.screenshotIdenticalToBaseline('footer.footer')
-			.end();
+			.assert.screenshotIdenticalToBaseline('footer.footer')
 	});
-	//after(browser => browser.end());
+
+
+	it('Header UI appears correct', function (browser) {
+		browser
+			.waitForElementVisible('header#header-wrapper')
+			.assert.screenshotIdenticalToBaseline('header#header-wrapper', 'homepage-header-neutral-state')
+	});
+
+	it('Header UI appears correct with menu expanded', function (browser) {
+		browser
+			.waitForElementVisible('header#header-wrapper')
+			.moveToElement('#site-header--menu-wrapper .menu > li:nth-child(1) > a', 10, 10)
+			.assert.screenshotIdenticalToBaseline('header#header-wrapper', 'homepage-header-menu-expanded-state')
+	});
+
+	it('Sock UI appears correct', function (browser) {
+		browser
+			.waitForElementVisible('aside.sock.sock-standard')
+			.assert.screenshotIdenticalToBaseline('aside.sock.sock-standard')
+	});
+
+
+
+	after(browser => browser.end());
 });
