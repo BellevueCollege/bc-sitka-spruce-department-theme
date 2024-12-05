d4a52c7eb7849d7e6f3cca6e19ceab63af28c7a5
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-12-05 15:03:01 -0800

    Add Application Guide Pattern

---
+++ patterns/page-application-guide.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Title: Application Guide Template
+ * Slug: bc-sitka-spruce/page-application-guide
+ * Categories: page, featured
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page, wp_template
+ * Viewport width: 1445
+ */
+?>
+<!-- wp:mayflower-blocks/alert {"alertClass":"danger","activeAlert":"danger","align":"wide"} -->
+<div class="wp-block-mayflower-blocks-alert alignwide alert alert-danger"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Setup Instructions:</h3>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item -->
+<li>In the Settings sidebar, go to Page Settings, and under 'Template' choose 'No Navigation Sidebar'</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Fill in content as needed</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Delete any blocks you don't need (including this one!)</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:mayflower-blocks/alert -->
+
+<!-- wp:bc-sitka-spruce/body-section -->
+<!-- wp:bc-sitka-spruce/body-section-content -->
+<!-- wp:heading -->
+<h2 class="wp-block-heading">General Guidelines</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Maecenas mattis pretium imperdiet. Morbi id lobortis mauris, vitae volutpat dui. Nullam vehicula metus in massa.</p>
+<!-- /wp:paragraph -->
+<!-- /wp:bc-sitka-spruce/body-section-content -->
+
+<!-- wp:bc-sitka-spruce/callout {"name":"bc-sitka-spruce/callout","data":{"display_callout":"1","_display_callout":"field_66b1334b05e6b","title":"Optional Callout","_title":"field_66b13323cdbd0","text":"Maecenas mattis pretium imperdiet. Morbi id lobortis mauris, vitae volutpat dui. Nullam vehicula metus in massa.","_text":"field_66b1337a05e6c","links":"","_links":"field_66b1338d05e6d","button":"","_button":"field_673bccabb8e8e"},"mode":"preview"} /-->
+<!-- /wp:bc-sitka-spruce/body-section -->
+
+<!-- wp:bc-sitka-spruce/application-steps-tabs {"title":"Application Steps","description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vel semper ridiculus lorem neque. Sem tempus sed odio amet, non a tristique blandit dictum."} -->
+<!-- wp:bc-sitka-spruce/tabcordion {"blockId":"tabcordion-a58cd1df-b874-4499-9a01-a91043eb6467","headingLevel":"h3","displayHeadingsVisually":false,"_allowedBlocks":["bc-sitka-spruce/application-step-single"],"_wrapContent":false,"_hideFormats":true} -->
+<!-- wp:bc-sitka-spruce/tabcordion-list -->
+<!-- wp:bc-sitka-spruce/tabcordion-list-tab {"tabActive":true,"tabId":"c997ba31-e8a5-4915-a7af-b952317be01e","tabTitle":"Student Type 1","tabDefault":true} /-->
+<!-- /wp:bc-sitka-spruce/tabcordion-list -->
+
+<!-- wp:bc-sitka-spruce/tabcordion-content -->
+<!-- wp:bc-sitka-spruce/tabcordion-content-panel {"tabActive":true,"tabDefault":true,"tabId":"c997ba31-e8a5-4915-a7af-b952317be01e","tabTitle":"Student Type 1"} -->
+<!-- wp:bc-sitka-spruce/application-step-single -->
+<div class="wp-block-bc-sitka-spruce-application-step-single application-step-single"><div class="container-xl"><div class="row"><!-- wp:bc-sitka-spruce/application-step-single-content {"heading":"Step 1: Step Title"} -->
+<div class="wp-block-bc-sitka-spruce-application-step-single-content application-step-single-content col-md-8"><h4 class="application-step-single-heading h2">Step 1: Step Title</h4><!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:bc-sitka-spruce/application-step-single-content -->
+
+<!-- wp:bc-sitka-spruce/callout {"name":"bc-sitka-spruce/callout","mode":"preview"} /--></div></div></div>
+<!-- /wp:bc-sitka-spruce/application-step-single -->
+
+<!-- wp:bc-sitka-spruce/application-step-single -->
+<div class="wp-block-bc-sitka-spruce-application-step-single application-step-single"><div class="container-xl"><div class="row"><!-- wp:bc-sitka-spruce/application-step-single-content {"heading":"Step 2: Step Title"} -->
+<div class="wp-block-bc-sitka-spruce-application-step-single-content application-step-single-content col-md-8"><h4 class="application-step-single-heading h2">Step 2: Step Title</h4><!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:bc-sitka-spruce/application-step-single-content -->
+
+<!-- wp:bc-sitka-spruce/callout {"name":"bc-sitka-spruce/callout","mode":"preview"} /--></div></div></div>
+<!-- /wp:bc-sitka-spruce/application-step-single -->
+<!-- /wp:bc-sitka-spruce/tabcordion-content-panel -->
+<!-- /wp:bc-sitka-spruce/tabcordion-content -->
+<!-- /wp:bc-sitka-spruce/tabcordion -->
+<!-- /wp:bc-sitka-spruce/application-steps-tabs -->
+
+<!-- wp:bc-sitka-spruce/accordion-section -->
+<!-- wp:bc-sitka-spruce/accordion-section-content {"title":"More Info"} -->
+<!-- wp:mayflower-blocks/collapsibles {"currentBlockClientId":"3eeb8244-33b7-4cd9-88d9-2313bc38afe2","isBootstrap5":true,"lock":{"move":false,"remove":false}} -->
+<div class="wp-block-mayflower-blocks-collapsibles accordion" id="accordion_3eeb8244-33b7-4cd9-88d9-2313bc38afe2"><!-- wp:mayflower-blocks/collapse {"currentBlockClientId":"ffc42db3-fe3e-40a3-98ec-c70b41cf572d","parentBlockClientId":"3eeb8244-33b7-4cd9-88d9-2313bc38afe2","isBootstrap5":true} -->
+<div class="wp-block-mayflower-blocks-collapse accordion-item"><h3 class="accordion-header mb-0" id="heading_ffc42db3-fe3e-40a3-98ec-c70b41cf572d"><button class="accordion-button bg-default text-bg-default
+		 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse_ffc42db3-fe3e-40a3-98ec-c70b41cf572d" aria-expanded="false" aria-controls="collapse_ffc42db3-fe3e-40a3-98ec-c70b41cf572d"></button></h3><div id="collapse_ffc42db3-fe3e-40a3-98ec-c70b41cf572d" class="accordion-collapse collapse " aria-labelledby="heading_ffc42db3-fe3e-40a3-98ec-c70b41cf572d" data-parent="#accordion_3eeb8244-33b7-4cd9-88d9-2313bc38afe2" data-bs-parent="#accordion_3eeb8244-33b7-4cd9-88d9-2313bc38afe2"><div class="accordion-body bg-default text-bg-default
+		"></div></div></div>
+<!-- /wp:mayflower-blocks/collapse -->
+
+<!-- wp:mayflower-blocks/collapse {"currentBlockClientId":"a518b84b-55e2-459b-9ebb-0199beecac81","parentBlockClientId":"3eeb8244-33b7-4cd9-88d9-2313bc38afe2","isBootstrap5":true} -->
+<div class="wp-block-mayflower-blocks-collapse accordion-item"><h3 class="accordion-header mb-0" id="heading_a518b84b-55e2-459b-9ebb-0199beecac81"><button class="accordion-button bg-default text-bg-default
+		 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse_a518b84b-55e2-459b-9ebb-0199beecac81" aria-expanded="false" aria-controls="collapse_a518b84b-55e2-459b-9ebb-0199beecac81"></button></h3><div id="collapse_a518b84b-55e2-459b-9ebb-0199beecac81" class="accordion-collapse collapse " aria-labelledby="heading_a518b84b-55e2-459b-9ebb-0199beecac81" data-parent="#accordion_3eeb8244-33b7-4cd9-88d9-2313bc38afe2" data-bs-parent="#accordion_3eeb8244-33b7-4cd9-88d9-2313bc38afe2"><div class="accordion-body bg-default text-bg-default
+		"></div></div></div>
+<!-- /wp:mayflower-blocks/collapse --></div>
+<!-- /wp:mayflower-blocks/collapsibles -->
+<!-- /wp:bc-sitka-spruce/accordion-section-content -->
+
+<!-- wp:bc-sitka-spruce/callout {"name":"bc-sitka-spruce/callout","mode":"preview"} /-->
+<!-- /wp:bc-sitka-spruce/accordion-section -->
+
+<!-- wp:bc-sitka-spruce/card-section {"title":"Additional Resources"} -->
+<!-- wp:bc-sitka-spruce/card-section-card /-->
+
+<!-- wp:bc-sitka-spruce/card-section-card /-->
+
+<!-- wp:bc-sitka-spruce/card-section-card /-->
+<!-- /wp:bc-sitka-spruce/card-section -->
+
+<!-- wp:bc-sitka-spruce/announcement-banner {"name":"bc-sitka-spruce/announcement-banner","data":{"title":"Upcoming Deadline","_title":"field_66e4c35bcf5ea","description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vel semper ridiculus lorem neque. Sem tempus sed odio amet, non a tristique blandit dictum. ","_description":"field_66e4c38bcf5eb","image":"","_image":"field_66e4c50d2ef34","button":"","_button":"field_66e4c39ecf5ec","links":"","_links":"field_66e4c40bcf5ed"},"mode":"auto"} /-->
+
+<!-- wp:bc-sitka-spruce/support-feature {"heading":"Support Available","sectionId":"support-feature-eedf5ec6-30e6-46af-b045-cafa0ad15e6b"} /-->
+
+<!-- wp:bc-sitka-spruce/department-feature {"title":"Department Feature"} /-->
+
+<!-- wp:bc-sitka-spruce/contact-selector {"name":"bc-sitka-spruce/contact-selector","data":{"title":"Make a Plan with an Advisor","_title":"field_66a7e30d7a047","description":"","_description":"field_66a7e33a7a048","profiles":"","_profiles":"field_669061d99346c"},"mode":"preview"} /-->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
