ecc2d5050125a724eb3df2da18724169dabbea44
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-12-11 09:57:02 -0800

    Display Front-End Warning when No Matching Program Found

--- src/blocks/template-program-info/render.php
+++ src/blocks/template-program-info/render.php
@@ -10,5 +10,10 @@ if ( ! array_key_exists( 'post', $context ) ) {
 }
 
 $program = Program::get_single_from_core_by_title( $context['post']->post_title );
+
+if ( ! $program ) {
+	echo '<p class="alert alert-danger container">' . __( 'Warning: Program not found in the main Program Directory. Please report this to the website manager.', 'bc-sitka-spruce' ) . '</p>';
+	return;
+}
 // Render Twig Template
 Timber::render( '/stories/program-info/program-info.twig', array_merge( $context, $program ) ); // Merge in the content from the core
