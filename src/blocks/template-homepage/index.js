af79964d5fd27443151f05f21e385f0f03a2efe7
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-12-20 11:58:03 -0800

    Display Warning if Homepage Pattern is Used on Non-Homepage

--- src/blocks/template-homepage/index.js
+++ src/blocks/template-homepage/index.js
@@ -2,6 +2,9 @@ import { __ } from '@wordpress/i18n';
 import { registerBlockType } from '@wordpress/blocks';
 import { useBlockProps } from '@wordpress/block-editor';
 import { InnerBlocks } from '@wordpress/block-editor';
+import { useState } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
+
 
 import './editor.scss';
 import './style.scss';
@@ -18,9 +21,35 @@ registerBlockType( 'bc-sitka-spruce/template-homepage', {
             className: 'template-homepage alignwide'
         });
 
+
+		// Detect if the current page is the front page
+		const { frontPageId, currentPostId } = useSelect((select) => {
+			return {
+				frontPageId: select("core").getEntityRecord('root', 'site')?.page_on_front,
+				currentPostId: select( 'core/editor' ).getCurrentPostId(),
+			};
+		});
+
+		// Set the isFrontPage state
+		const [ isFrontPage, setIsFrontPage ] = useState( undefined );
+		if ( isFrontPage === undefined && frontPageId !== undefined && currentPostId !== undefined ) {
+			if ( frontPageId === currentPostId ) {
+				setIsFrontPage( true );
+			} else {
+				setIsFrontPage( false );
+			}
+		}
+
         return (
             <>
                 <div { ...blockProps }>
+					{ isFrontPage === false && (
+						<div className="container-xl card text-bg-warning">
+							<h2 className="h5"><strong>{__("Warning!", "bc-sitka-spruce")}</strong> {__("You are using a homepage template on a page that is not set as your site's homepage!", "bc-sitka-spruce")}</h2>
+							<p>{__("The homepage template is only intended to be used on the homepage of the site. To prevent major visual issues, only use this pattern on the homepage of your site.", "bc-sitka-spruce")}</p>
+							<p>{__("To change the homepage of your site to a different page, please submit a ticket to the ITS Service Desk.", "bc-sitka-spruce")}</p>
+						</div>
+					) }
                     <InnerBlocks
                         template={ BLOCK_TEMPLATE }
                         templateLock="all"
