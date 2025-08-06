edc05dd7a7df0b1b9cf2c26d345b559fc67e0dd1
efuenzalida52 <efuenzalida52@gmail.com>
2025-08-05 17:02:45 -0700

    Fixed issue of small stories appearing on FE when not selected

--- src/blocks/posts-feature/render.php
+++ src/blocks/posts-feature/render.php
@@ -2,18 +2,37 @@
 use BcSitkaSpruce\Controllers\Posts;
 use Timber\Timber;
 $context                      = Timber::context();
+// Section Heading & Description
 $context['heading']           = esc_html( $attributes['title'] );
 $context['subheading']        = esc_html( $attributes['description'] );
+//Optional CTA Link
 $context['link_custom']       = $attributes['linkTitle'] && $attributes['linkUrl'] ? array(
 	'title' => esc_html( $attributes['linkTitle'] ),
 	'url'   => esc_url( $attributes['linkUrl'] ),
 ) : null;
 
 // Note: naming doesn't match with posts because this reuses the News story for rendering
+// Featured/Large Story
 $context['large_story_id']    = esc_attr( $attributes['largeStoryId'] );
-$context['small_story_types'] = $attributes['smallStoryTypes'];
 $context['featured_news']     = Posts::get_single( $attributes['largeStoryId'] );
-$context['news_listing']      = Posts::get_by_category( terms: $attributes['smallStoryTypes'], exclude: array( $attributes['largeStoryId'] ), limit: 3 );
+
+// Small Story Types
+$context['small_story_types'] = $attributes['smallStoryTypes'];
+
+//Conditional for when to grab small stories
+//$context['news_listing']      = Posts::get_by_category( terms: $attributes['smallStoryTypes'], exclude: array( $attributes['largeStoryId'] ), limit: 3 );
+if ( ! empty( $attributes['smallStoryTypes'] ) ) {
+	$context['news_listing'] = Posts::get_by_category(
+		terms: $attributes['smallStoryTypes'],
+		exclude: [ $attributes['largeStoryId'] ],
+		limit: 3
+	);
+} else {
+	$context['news_listing'] = []; //purposely empty
+	error_log( 'No Small Story Types selected. Skipping small posts' );
+}
+
+// Check before rendering
 if ( $context['large_story_id'] || count( $context['small_story_types'] ) > 0 ) {
 	Timber::render( '/stories/news-feature/news-feature.twig', $context );
 } else {
