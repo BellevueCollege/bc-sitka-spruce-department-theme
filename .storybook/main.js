5a9e3e789f20ce9decbcad1ad41650314104dd46
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2025-10-06 14:44:04 -0700

    👌 IMPROVE: Get Storybook Working for Includes
    Add basic namespacing and update stories to use it, while moving to a new twig loader for vite.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
-import twig from 'vite-plugin-twig-drupal';
+// .storybook/main.js
+import twig from 'vite-plugin-twigjs-loader';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
-// Define __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -10,22 +10,17 @@ const __dirname = dirname(__filename);
 const config = {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   staticDirs: ["../assets/img"],
-
-  async viteFinal(config, { configType }) {
-    config.plugins = config.plugins || [];
-
-    config.plugins.push(
+  async viteFinal(viteConfig) {
+    viteConfig.plugins ||= [];
+    viteConfig.plugins.push(
       twig({
-        root: join(__dirname, '..'),
         namespaces: {
-          'components': join(__dirname, '../stories'),
-          'stories': join(__dirname, '../stories'),
+          '@stories': join(__dirname, '../stories'),
+					'@views': join(__dirname, '../views'),
         },
-
       })
     );
-
-    return config;
+    return viteConfig;
   },
 
   addons: [
