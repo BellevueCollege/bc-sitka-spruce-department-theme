82c82d772096d512929a72a9629e2d50ade2053f
efuenzalida52 <efuenzalida52@gmail.com>
2025-11-20 16:06:50 -0800

    Anchor Support for Accordion Section #421
    Added anchor support for accordion section

--- src/blocks/accordion-section/render.php
+++ src/blocks/accordion-section/render.php
@@ -3,6 +3,9 @@ use Timber\Timber;
 
 $context            = Timber::context();
 $context['content'] = $content;
+$context['anchor'] = ! empty( $attributes['anchor'] )
+	? sanitize_title( $attributes['anchor'] )
+	: '';
 
 // Render Twig Template
 Timber::render( '/stories/accordion-section/accordion-section.twig', $context );
