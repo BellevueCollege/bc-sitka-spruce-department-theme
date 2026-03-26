21155a18e02743f89d4b49185231a82d612fdf23
efuenzalida52 <efuenzalida52@gmail.com>
2026-03-26 08:45:36 -0700

    Create Templates and Blocks for Agendas, Resolutions, and Action Items #453 #100 #8
    Created archive templates for all agenda components

---
+++ src/controllers/page-templates/archive-resolution.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Board of Trustees Agenda Archive Part
+ * @package BC Sitka Spruce Theme
+ */
+
+$context = Timber::context();
+$context['title'] = 'Board of Trustees Agendas'; // Fallback title
+
+//All Resolution(s)- ordered by the meeting date
+$args = array(
+    'post_type'      => 'resolution',
+    'posts_per_page' => -1, // Get all of them
+    'meta_key'       => 'date',
+    'orderby'        => 'meta_value',
+    'order'          => 'DESC',
+);
+$agendas = Timber::get_posts( $args );
+
+//Group them by Year (this is how they were grouped in douglas theme)
+$posts_by_year = array();
+foreach ( $agendas as $agenda ) {
+    $date = $agenda->meta('date');
+    if ( ! empty( $date ) ) {
+        // Extract just the year from the ACF date field
+        $year = gmdate( 'Y', strtotime( $date ) );
+        $posts_by_year[ $year ][] = $agenda;
+    }
+}
+
+//Sort so the years are in descending order (newest/latest to oldest/earliest)
+krsort( $posts_by_year );
+//Pass  to Twig
+$context['posts_by_year'] = $posts_by_year;
+
+//Render
+Timber::render( 'archive-agendas.twig', $context );
