0434a86e1b7a2d8e0f11a87fb2a354176751791c
efuenzalida52 <efuenzalida52@gmail.com>
2026-05-07 12:22:16 -0700

    Updated to use namespaces Removed/moved accidental acf file fixed styling to used var

--- src/controllers/page-templates/single-agendas.php
+++ src/controllers/page-templates/single-agendas.php
@@ -6,6 +6,7 @@
 $context = Timber::context();
 $timber_post = Timber::get_post();
 $context['post'] = $timber_post;
+$loader = new Timber\Loader();
 
 // Render twig
 Timber::render( 'content/single-agendas.twig', $context );
