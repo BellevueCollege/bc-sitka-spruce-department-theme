e12a4d99635707463263ca5ed4d7df8ea1cde6e1
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2026-06-03 13:30:12 -0700

    📦 NEW: WP E2E Testing Framework

---
+++ playwright.config.js
@@ -0,0 +1,8 @@
+// playwright.config.js
+import { defineConfig } from '@playwright/test';
+import baseConfig from '@wordpress/scripts/config/playwright.config.js';
+
+export default defineConfig({
+    ...baseConfig,
+    testDir: './tests/e2e'
+});
