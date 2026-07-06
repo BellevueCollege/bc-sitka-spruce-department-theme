6400210321d0c2a1e60dbed6fce41bf8cf3b8c4e
efuenzalida52 <efuenzalida52@gmail.com>
2026-07-06 09:35:28 -0700

    Removed unneeded files

--- src/blocks/bootstrap-accordion/index.js
+++
@@ -1,8 +0,0 @@
-import { registerBlockType } from '@wordpress/blocks';
-import metadata from './block.json';
-
-registerBlockType(metadata.name, {
-    ...metadata,
-    edit: () => null, // ACF hooks the visual preview window onto render.php
-    save: () => null, // Content fields pulled via get_field database arrays
-});
